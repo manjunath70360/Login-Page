0852275848926ba5f2a46ee0ba208059ea48a3b0
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-05-10 11:45:46 +0530

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,18 @@
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
+
 import Login from "./components/login"
+import SuccessLogin "./components/SuccessLogin"
 import "./App.css"
 
-const App = ()=> <Login />
+const App = ()=> {
+return(
+      <BrowserRouter/>
+      <Switch>
+          <Route exact path="/" component={SuccessLogin} />
+          <Route exact path="/login" component={LoginForm} />
+      </Switch>
+      </BrowserRouter>
+)
+}
 
 export default App;
