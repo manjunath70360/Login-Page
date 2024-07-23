5efeb9e6c17c6ac6ba38592552d8a05d39ea1370
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-07-23 11:18:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const jwt = require("jsonwebtoken")
-
+const jwt = require("jsonwebtoken");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
-
 const cors = require("cors");
 const path = require("path");
 
@@ -18,8 +16,6 @@ app.use(cors({
 }));
 
 const dbPath = path.join(__dirname, "database.db");
-
-
 
 let db = null;
 
@@ -36,7 +32,6 @@ const initializeDBAndServer = async () => {
       console.log(`Server running on port ${PORT}`);
     });
 
-    
   } catch (err) {
     console.log(`DB Error: ${err.message}`);
     process.exit(1);
@@ -62,7 +57,7 @@ const createUserTable = async () => {
 };
 
 app.get('/', (req, res) => {
-    res.send('User api running new deploy');
+  res.send('User API running new deploy');
 });
 
 app.post("/login", async (request, response) => {
@@ -70,29 +65,28 @@ app.post("/login", async (request, response) => {
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
   const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
-    response.send({statusCode:400, text:"Invalid user"})
+    response.status(400).json({ statusCode: 400, text: "Invalid user" });
   } else {
-    const isPasswordMatched = await bcrypt.compare(password, dbUser.password)
-
-    if (isPasswordMatched === true) {
-      const payload = {username:username}
-      const jwtToken = jwt.sign(payload, "my_secret_token")
-      response.send({jwtToken:jwtToken,statusCode:200, text:"Logged In"})
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "my_secret_token");
+      response.status(200).json({ jwtToken: jwtToken, statusCode: 200, text: "Logged In" });
     } else {
-      response.send({statusCode:400, text:"Invalid Password"})
+      response.status(400).json({ statusCode: 400, text: "Invalid Password" });
     }
   }
 });
 
 app.post("/newuser", async (request, response) => {
-  const { username,  password ,phoneNo,address} = request.body;
-  const hashedPassword = await bcrypt.hash(password, 10)
+  const { username, password, phoneNo, address } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
   const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
     const createUserQuery = `
       INSERT INTO 
-        user (username, password, phoneNum,address) 
+        user (username, password, phoneNum, address) 
       VALUES 
         (
           '${username}', 
@@ -102,12 +96,10 @@ app.post("/newuser", async (request, response) => {
         )`;
     const dbResponse = await db.run(createUserQuery);
     const newUserId = dbResponse.lastID;
-    response.send({statusCode:200, text:`Created new user with id ${newUserId}`});
+    response.status(200).json({ statusCode: 200, text: `Created new user with id ${newUserId}` });
   } else {
-    response.send({statusCode:400, text:"user already exit"})
+    response.status(400).json({ statusCode: 400, text: "User already exists" });
   }
 });
-
-
 
 module.exports = app;
